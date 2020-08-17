f1bf9d808772ced560b73a6f9e9adc0749a0290d
Emily Zhai <emily.zhai@convoy.com>
2020-08-17 15:24:51 -0700

    page factory on about page

--- app/pages/about.tsx
+++ app/pages/about.tsx
@@ -1,12 +1,32 @@
 import React from 'react';
-import { PageTitle } from '~/components';
+import axios from 'axios';
 
-const About = () => {
+import { PageTitle, PageFactory } from '~/components';
+import { PageSection } from '~/shared/types';
+import routes from '~/shared/routes';
+
+interface Props {
+  content: PageSection[];
+}
+
+const About = (props: Props) => {
+  const { content } = props;
   return (
     <>
       <PageTitle>About Us</PageTitle>
+      { content.map(section => <PageFactory key={section.id} section={section} />) }
     </>
   );
+};
+
+export const getStaticProps = async () => {
+  const cmsRoute = routes.contentRoute(routes.about);
+  const { data } = await axios.get(cmsRoute);
+  return {
+    props: {
+      content: data[0].content,
+    },
+  };
 };
 
 export default About;
