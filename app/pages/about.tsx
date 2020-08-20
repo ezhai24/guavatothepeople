413655858b1d7ab795a505bbfe269d0947d584a6
Emily Zhai <emily.zhai@convoy.com>
2020-08-19 17:13:57 -0700

    fetching page content at runtime to support cms updates

--- app/pages/about.tsx
+++ app/pages/about.tsx
@@ -1,32 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 import { PageTitle, PageFactory } from '~/components';
-import { PageSection } from '~/shared/types';
 import routes from '~/shared/routes';
 
-interface Props {
-  content: PageSection[];
-}
+const About = () => {
+  const [content, setContent] = useState([]);
+  
+  useEffect(() => {
+    const getPageContent = async () => {
+      const cmsRoute = routes.contentRoute(routes.about);
+      const { data } = await axios.get(cmsRoute);
+      setContent(data[0].content);
+    };
+    getPageContent();
+  }, []);
 
-const About = (props: Props) => {
-  const { content } = props;
   return (
     <>
       <PageTitle>About Us</PageTitle>
       { content.map(section => <PageFactory key={section.id} section={section} />) }
     </>
   );
-};
-
-export const getStaticProps = async () => {
-  const cmsRoute = routes.contentRoute(routes.about);
-  const { data } = await axios.get(cmsRoute);
-  return {
-    props: {
-      content: data[0].content,
-    },
-  };
 };
 
 export default About;
--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -1,27 +1,22 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 import { PageFactory } from '~/components';
-import { PageSection } from '~/shared/types';
 import routes from '~/shared/routes';
 
-interface Props {
-  content: PageSection[];
-}
+const Home = () => {
+  const [content, setContent] = useState([]);
+  
+  useEffect(() => {
+    const getPageContent = async () => {
+      const cmsRoute = routes.contentRoute(routes.home);
+      const { data } = await axios.get(cmsRoute);
+      setContent(data[0].content);
+    };
+    getPageContent();
+  }, []);
 
-const Home = (props: Props) => {
-  const { content } = props;
   return content.map((section, index) => <PageFactory key={index} section={section} />);
-};
-
-export const getStaticProps = async () => {
-  const cmsRoute = routes.contentRoute(routes.home);
-  const { data } = await axios.get(cmsRoute);
-  return {
-    props: {
-      content: data[0].content,
-    },
-  };
 };
 
 export default Home;
