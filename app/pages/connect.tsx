41ec8b8df51861065a05eee46dca91f5d4e26b99
Emily Zhai <emily.zhai@convoy.com>
2020-08-21 19:28:28 -0700

    setting up connect page

--- app/pages/connect.tsx
+++ app/pages/connect.tsx
@@ -1,9 +1,26 @@
-import React from 'react';
-import { PageTitle } from '~/components';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+import { PageTitle, PageFactory } from '~/components';
+import routes from '~/shared/routes';
 
 const Connect = () => {
+  const [content, setContent] = useState([]);
+  
+  useEffect(() => {
+    const getPageContent = async () => {
+      const cmsRoute = routes.contentRoute(routes.connect);
+      const { data } = await axios.get(cmsRoute);
+      setContent(data[0].content);
+    };
+    getPageContent();
+  }, []);
+
   return (
-    <PageTitle>Connect</PageTitle>
+    <>
+      <PageTitle>Connect</PageTitle>
+      { content.map(section => <PageFactory key={section.id} section={section} />) }
+    </>
   );
 };
 
