a89af24132c788ddb163faa2681f94fa848da5b5
Emily Zhai <emily.zhai@convoy.com>
2020-08-22 14:54:00 -0700

    setting up donate page

--- app/pages/donate.tsx
+++ app/pages/donate.tsx
@@ -1,9 +1,26 @@
-import React from 'react';
-import { PageTitle } from '~/components';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+import { PageTitle, PageFactory } from '~/components';
+import routes from '~/shared/routes';
 
 const Donate = () => {
+  const [content, setContent] = useState([]);
+  
+  useEffect(() => {
+    const getPageContent = async () => {
+      const cmsRoute = routes.contentRoute(routes.donate);
+      const { data } = await axios.get(cmsRoute);
+      setContent(data[0].content);
+    };
+    getPageContent();
+  }, []);
+
   return (
-    <PageTitle>Donate</PageTitle>
+    <>
+      <PageTitle>Donate</PageTitle>
+      {content.map((section, index) => <PageFactory key={index} section={section} />)}
+    </>
   );
 };
 
