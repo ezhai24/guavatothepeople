2df657c05c0e1a1838e2fee3d10346849cdec563
Emily Zhai <emily.zhai@convoy.com>
2020-08-29 00:36:47 -0700

    setting up blm page

--- app/pages/blm.tsx
+++ app/pages/blm.tsx
@@ -1,9 +1,26 @@
-import React from 'react';
-import { PageTitle } from '~/components';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+import { PageTitle, PageFactory } from '~/components';
+import routes from '~/shared/routes';
 
 const BLM = () => {
+  const [content, setContent] = useState([]);
+  
+  useEffect(() => {
+    const getPageContent = async () => {
+      const cmsRoute = routes.contentRoute(routes.blmResources);
+      const { data } = await axios.get(cmsRoute);
+      setContent(data[0].content);
+    };
+    getPageContent();
+  }, []);
+
   return (
-    <PageTitle>BLM Resources</PageTitle>
+    <>
+      <PageTitle>BLM Resources</PageTitle>
+      {content.map(section => <PageFactory key={section.id} section={section} />)}
+    </>
   );
 };
 
